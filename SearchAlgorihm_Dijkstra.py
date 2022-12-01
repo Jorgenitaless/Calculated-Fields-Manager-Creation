e02e8506eb487416cd1601379cf787bb6362afe4
Jorgenitaless <117989424+Jorgenitaless@users.noreply.github.com>
2022-12-01 15:51:57 +0100

    Add files via upload

---
+++ SearchAlgorihm_Dijkstra.py
@@ -0,0 +1,45 @@
+
+import matplotlib.pyplot as plt
+
+graph = {'Worker':{'Supervisory':1,'c':1},'Supervisory':{'Worker':1,'c':1,'d':1},'c':{'Supervisory':1,'d':1,'e':1},'d':{'c':1,'e':1},'e':{'d':1},'f':{'d':1}}
+ 
+
+def dijkstra(graph,start,goal):
+    shortest_distance = {}
+    predecessor = {}
+    unseenNodes = graph
+    infinity = 9999999
+    path = []
+    for node in unseenNodes:
+        shortest_distance[node] = infinity
+    shortest_distance[start] = 0
+ 
+    while unseenNodes:
+        minNode = None
+        for node in unseenNodes:
+            if minNode is None:
+                minNode = node
+            elif shortest_distance[node] < shortest_distance[minNode]:
+                minNode = node
+ 
+        for childNode, weight in graph[minNode].items():
+            if weight + shortest_distance[minNode] < shortest_distance[childNode]:
+                shortest_distance[childNode] = weight + shortest_distance[minNode]
+                predecessor[childNode] = minNode
+        unseenNodes.pop(minNode)
+ 
+    currentNode = goal
+    while currentNode != start:
+        try:
+            path.insert(0,currentNode)
+            currentNode = predecessor[currentNode]
+        except KeyError:
+            print('Path not reachable')
+            break
+    path.insert(0,start)
+    if shortest_distance[goal] != infinity:
+        print('Shortest distance is ' + str(shortest_distance[goal]))
+        print('The path is ' + str(path))
+ 
+ 
+dijkstra(graph, 'f', 'Worker')
