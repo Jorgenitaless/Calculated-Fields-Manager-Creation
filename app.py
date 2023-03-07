db93f8646947bca8bf92e849b6d23adab2e3d85a
clxudiajazmin <claudia_soria@bnetbuilders.com>
2023-03-07 13:09:47 +0100

    Aplicación funcionando

--- app.py
+++ app.py
@@ -9,10 +9,15 @@ def start():
     if request.method == 'POST':
         source = request.form.get('source')
         target = request.form.get('target')
-        result = source + target
+        result = source + ' --> ' + target
         return render_template('sol.html', result = result)
     return render_template("Chatgpt.html")
 
 
+@app.route('/sol/')
+def sol():
+    return render_template('sol.html')
+
+        
 def find_path(G, source, target):
     nx.shortest_path(G, source='Worker', target = 'Dependent')
