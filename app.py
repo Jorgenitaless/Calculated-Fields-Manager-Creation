67cff4aff1d4fed975522e3961a4ed97c39337af
clxudiajazmin <claudia_soria@bnetbuilders.com>
2023-03-08 12:51:36 +0100

    Pagina funcionando con Flask

--- app.py
+++ app.py
@@ -5,20 +5,21 @@ from flask import Flask, request, render_template, abort
 app = Flask(__name__)
 
 G = nx.read_gpickle('parts.gpickle')
+
+
 @app.route('/', methods=['GET', 'POST'])
 def start():
     if request.method == 'POST':
         source = request.form.get('source')
         target = request.form.get('target')
+        
         result = nx.shortest_path(G, source=source, target = target)
+        
         return render_template('sol.html', result = result)
-    return render_template("Chatgpt.html")
+    
+    return render_template("index.html")
 
 
 @app.route('/sol/')
 def sol():
     return render_template('sol.html')
-
-        
-def find_path(G, source, target):
-    nx.shortest_path(G, source='Worker', target = 'Dependent')
